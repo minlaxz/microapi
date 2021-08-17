7fac14fd0a4e2c9186f432f2b3f9dbcdd2853be2
minlaxz <minminlaxz@gmail.com>
2021-08-17 22:49:24 +0630

    Hmm cors is noice

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express');
-const githubRoute = require('./routes/github');
+const githubRoute = require('./api/github');
+const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
+const base = '/api';
+const apiVersion = '/v1';
 
-app.get('/', async (req, res) => {
+app.get('/', cors(), async (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-app.use('/api/v1/github', githubRoute);
+app.get(`${base}`, async (req, res) => {
+    res.status(302).redirect(`${base}${apiVersion}`);
+})
 
+app.get(`${base}${apiVersion}`, async (req, res) => {
+    res.status(302).redirect(`${base}${apiVersion}/doc`);
+})
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port 3000!`);
+app.get(`${base}${apiVersion}/doc`, async (req, res) => {
+    res.status(200).send('Documentation');
+})
+
+app.use(`${base}${apiVersion}/github`, githubRoute);
+
+app.get("*", async (req, res) => {
+    res.status(404).send('Not Found');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 })
 
 module.exports = app;
