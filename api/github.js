915427519e1ab35187b1c5ff99d92c52475eac2a
minlaxz <minminlaxz@gmail.com>
2021-08-19 02:53:08 +0630

    Handling Errors completed

--- api/github.js
+++ api/github.js
@@ -10,62 +10,65 @@ router.use(cors({
     headers: ['Content-Type', 'Accept, X-Requested-With']
 }))
 
-// error handler middleware
-router.use((error, req, res, next) => {
-    // if (!error.statusCode) error.statusCode = 500;
-    // if (error.statusCode === 301) {
-    //     return res.status(301).redirect('/not-found');
-    // }
-    // return res.status(error.statusCode).json({ message: error.message.toString() });
-    return res.status(error.status || 500).send({
-        error: {
-            status : error.status || 500,
-            message: error.message || 'Internal Server Error'
-        },
-    });
-});
-
 router.use('/:action', (req, res, next) => {
     if (req.params.action === 'lastcommit') {
         let { repo } = req.query
         if (!repo) {
-            return res.status(400).json({ message: "Missing repo parameter" })
+            // throw new Error('BROKEN')
+            const error = new Error('Missing repo query');
+            error.status = 400;
+            next(error);
+        } else{
+            next();
         }
-        next()
+    } else {
+        const error = new Error(`Invalid action : ${req.params.action}`);
+        error.status = 400;
+        next(error);
     }
-})
+});
 
 router.get('/', async (req, res) => {
     const returnBody = { message: "Eh this is github route" };
     res.status(200).json(returnBody);
-})
+});
 
 router.get('/:action', async (req, res, next) => {
-    if (req.params.action === 'lastcommit') {
-        let { user, repo, branch } = req.query;
-        !user && (user = "minlaxz")
-        !branch && (branch = "main")
-        const url = `https://api.github.com/repos/${user}/${repo}/branches/${branch}`
-        try {
-            const response = await axios.get(url, {
-                headers: { 'User-Agent': 'curl/7.68.0' }
-            });
-
-            const returnBody = {
-                ghUser: user,
-                ghRepo: repo,
-                ghBranch: branch,
-                data: response.data.commit.sha
-            }
-            return res.status(200).json(returnBody)
+    let { user, repo, branch } = req.query;
+    !user && (user = "minlaxz")
+    !branch && (branch = "main")
+    const url = `https://api.github.com/repos/${user}/${repo}/branches/${branch}`
+    try {
+        const response = await axios.get(url, {
+            headers: { 'User-Agent': 'curl/7.68.0' }
+        });
+        const returnBody = {
+            ghUser: user,
+            ghRepo: repo,
+            ghBranch: branch,
+            data: response.data.commit.sha
         }
-        catch (error){
-            next(error);
-        }
-    } else {
-        return res.status(400).json({ message: `I don't know ${req.params.action} params` })
+        return res.status(200).json(returnBody)
+    }
+    catch (error) {
+        if (!error.status) error.status = 404
+        next(error);
     }
 
+});
+
+// error handler middleware
+router.use((error, req, res, next) => {
+    console.error(error.stack)
+    if (res.headersSent) {
+        return next(error)
+    }
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || 'Internal Server Error'
+        },
+    });
 });
 
 module.exports = router;
